358e2eea24412bc4ad84306aef784bac3720365e
Kshitiz Udainiya <kshitizudainiya@gmail.com>
2020-03-30 19:56:51 +0530

    added my shape

--- Problem1/Ring.cpp
+++ Problem1/Ring.cpp
@@ -6,10 +6,20 @@
 #include <GLFW/glfw3.h>
 
 // just change n for better approx circle
-const int n=20;
-void DrawCircle(float vertices[3*n], float ori[3], float r)
+// change various attributes of ring
+const int n=30;
+
+float center[] = { 0.0f, 0.0f, 0.0f};
+
+float r1= 0.6f;
+
+float r2= 0.4f;
+
+
+
+void DrawCircle(float vertices[2*3*n], float ori[3], float r,int a)
 {
-    for(int i=0;i<n;i++)
+    for(int i=a;i<a+n;i++)
     {
         double a=3.14*2*i/n;
         float om[3]={0.0f,0.0f,0.0f};
@@ -25,6 +35,8 @@ void DrawCircle(float vertices[3*n], float ori[3], float r)
 }
 const GLint WIDTH = 600, HEIGHT = 600;
 
+
+//source code of vshaders and fshaders
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -49,7 +61,9 @@ void framebuffer_size_callback (GLFWwindow* window, int width, int height)
 
 
 int main(){
-   
+    
+    // setting up our window and initializing glew/glfw
+    
     glfwInit();
     
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
@@ -80,14 +94,34 @@ int main(){
     glViewport( 0,0,screenWidth,screenHeight);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
-    float vertices[3*n];
-    float center[] = { 0.0f, 0.0f, 0.0f};
-    float radius= 0.5f;
-    DrawCircle(vertices, center, radius);
-
-
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //creating vertices for our circle
     
+    float vertices[2*3*n];
+    
+    DrawCircle(vertices, center, r1,0);
+    DrawCircle(vertices,center,r2,n);
+    
+    //creating indices for the triangles
+    
+    unsigned int indices[2*3*n];
+    int t=0;
+    for(int i=0;i<n;i++)
+    {
+        indices[6*i]=t;
+        indices[6*i+1]=t+1;
+        indices[6*i+2]=n+t;
+        indices[6*i+3]=t+1;
+        indices[6*i+4]=n+t;
+        indices[6*i+5]=n+t+1;
+        t++;
+    }
+    indices[6*n-1]=0;
+    indices[6*n-2]=n-1;
+    
+    
+    //creating vertex shaders and fragment shaders
+    
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1,&vertexShaderSource,NULL);
     glCompileShader(vertexShader);
     //check success
@@ -117,6 +151,8 @@ int main(){
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog2 << std::endl;
     }
     
+    //creating shader program
+    
     int Shaeder;
     Shaeder=glCreateProgram();
     
@@ -135,36 +171,53 @@ int main(){
     glDeleteShader(vertexShader);
     glDeleteShader(FragmentShader);
     
+    //creating VAO,VBO,EBO
+    
     unsigned int VAO;
     glGenVertexArrays(1,&VAO);
     
-    unsigned int VBO;
+    unsigned int VBO,EBO;
     
     glGenBuffers(1,&VBO);
+    glGenBuffers(1,&EBO);
     
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
     
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
+    
+    
+    //enable to see only borders
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    
     while( !glfwWindowShouldClose( window ))
     {
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT);
         
         glUseProgram(Shaeder);
-
-        glDrawArrays(GL_LINE_LOOP, 0, n);
+        glDrawElements(GL_TRIANGLES, 6*n, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers( window);
         glfwPollEvents( );
     }
-
+    
+    
+    //unbinding and deleting
+    
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
+    
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1,&EBO);
     
     glfwTerminate( );
     return 0;
